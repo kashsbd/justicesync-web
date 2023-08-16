d205dba07b42a5412396412f6dcdd02bbcbd64fb
kash <parakash.sbd@gmail.com>
2023-08-15 22:15:45 -0500

    feat: added useQuery npm package

--- src/pages/clients/Clients.js
+++ src/pages/clients/Clients.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
+
 import CommonDrawer from "../../components/commondrawer/CommonDrawer";
 import Sidebar from "../../components/sidebar/Sidebar";
 import DetailComponent from "../../components/detailcomponent/DetailComponent";
-import { styled } from "@mui/material/styles";
+
 import { drawerWidth } from "../../utils/constants";
+
 import "./Clients.css";
-import { Box } from "@mui/material";
 
 const clientList = [
   { case: "this is client one" },
@@ -15,25 +18,27 @@ const clientList = [
   ,
 ];
 
-const Clients = () => {
-  const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
-    ({ theme, open }) => ({
-      flexGrow: 1,
-      padding: theme.spacing(3),
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
       transition: theme.transitions.create("margin", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
       }),
-      marginLeft: `-${drawerWidth}px`,
-      ...(open && {
-        transition: theme.transitions.create("margin", {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-      }),
-    })
-  );
+      marginLeft: 0,
+    }),
+  })
+);
+
+const Clients = () => {
+
   return (
     <Box sx={{ display: "flex" }}>
       <CommonDrawer />
