17171fface495e89166f9736c7d6714631543f1f
kash <parakash.sbd@gmail.com>
2023-08-16 01:00:17 -0500

    feat: staff's detail component is done

---
+++ src/hooks/useGetAllStaff.js
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+
+import $axios from "../lib/$axios";
+
+const getAllStaffData = async () => {
+  try {
+    const res = await $axios.get("users");
+    return res.data?.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const useGetAllStaff = () => {
+  return useQuery({ queryKey: ["users"], queryFn: getAllStaffData });
+};
+
+export default useGetAllStaff;
